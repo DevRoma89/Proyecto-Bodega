d672029da47907d0c528a978473d38eee9280a01
Augusto Ojeda <manuojedauniversidad@gmail.com>
2024-03-08 07:03:34 -0300

    c

--- src/MVC/Controlador/ControladorCiudad.java
+++ src/MVC/Controlador/ControladorCiudad.java
@@ -1,12 +1,17 @@
 
 package MVC.Controlador;
 
+import MVC.Modelo.Ciudad;
+import MVC.Modelo.DAO.CiudadDAO;
 import MVC.Vista.IF_Ciudad;
 
 
 public class ControladorCiudad extends IF_Ciudad {
-
-
+    Ciudad ciudad = new Ciudad();
+    CiudadDAO dao = new CiudadDAO();
+    
+    
+    
 
     
 }
